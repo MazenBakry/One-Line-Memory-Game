4d0cc408c3758022dfa4898102152c87ab2efd88
Mazen El Bakry <mazenkhaled62@gmail.com>
2022-03-14 21:15:45 +0200

    Updated Game to last version

--- OneLineMemoryGame.py
+++ OneLineMemoryGame.py
@@ -1,4 +1,4 @@
-# FCAI – Programming 1 – 2022 - Assignment 2\
+# FCAI – Programming 1 – 2022 - Assignment 2
 # Author: Mazen Khaled El Bakry
 # Program Name: OneLineMemoryGame
 # Last Modification Date: 14/3/2022
